f412e5375cdd120f7de18cb9adfefb59f445de96
lemondy97 <104114377+lemondy97@users.noreply.github.com>
2022-07-07 18:06:51 +0900

    20220707_project3_jingyu2
    서브 추가 및 업데이트

--- js/Scroll_Event_JQ.js
+++ js/Scroll_Event_JQ.js
@@ -1,20 +1,20 @@
             $(function(){
                 $(window).scroll(function(){
-                    var ws = $(this).scrollTop();
+                    var location = $(this).scrollTop();
                     var section3_box1 = $('.news_image_wrap').offset().top;
                     var section3_box2 = $('.small_news_box').offset().top;
                     var section4_box1 = $('.employ_box').offset().top;
                     var section4_box2 = $('.shop_box').offset().top;
-                    if(ws > section3_box1 - 700){
+                    if(location > section3_box1 - 700){
                         $('.news_image_wrap').css("transform", "translate(0px, 0px)").css("opacity", "1");
                     }
-                    if(ws > section3_box2 - 700){
+                    if(location > section3_box2 - 700){
                         $('.small_news_box').css("transform", "translate(0px, 0px)").css("opacity", "1");
                     }
-                    if(ws > section4_box1 - 700){
+                    if(location > section4_box1 - 700){
                         $('.employ_box').css("transform", "translate(0px, 0px)").css("opacity", "1");
                     }
-                    if(ws > section4_box2 - 700){
+                    if(location > section4_box2 - 700){
                         $('.shop_box').css("transform", "translate(0px, 0px)").css("opacity", "1");
                     }
                 });
