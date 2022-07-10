756cb3c2a15e1ab42cfc1bb2412c5b097863f345
lemondy97 <lemondy97@gmail.com>
2022-07-10 20:46:22 +0900

    20220708_project3_jingyu1
    서브 업데이트

---
+++ js/mobile_menu.js
@@ -0,0 +1,10 @@
+            $(document).ready(function(){
+                $('.menu_icon').click(function(){
+                    $('#reveal_menu').css('opacity', '1');
+                    $('#reveal_menu').css('width', '100%');
+                });
+                $('.disappear_menu').click(function(){
+                    $('#reveal_menu').css('opacity', '0');
+                    $('#reveal_menu').css('width', '0%');
+                });
+            });
